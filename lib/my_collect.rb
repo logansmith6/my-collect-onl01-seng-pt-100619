4ea5d1d1699847f98dc81854dd238efcd3e3843d
Logan Smith <logsmith06@gmail.com>
2019-10-18 22:23:42 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
 
+def my_collect(collection)
 
+counter = 0
+while counter < collection.size do
+
+  yield collection[counter]
+  counter += 1
+
+end
+return collection
+
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
